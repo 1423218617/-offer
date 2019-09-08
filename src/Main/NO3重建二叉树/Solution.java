d9b932e6f649491870c8f5c04f1356ba3b579edd
Administrator <hexi>
2019-09-08 19:26:29 +0800

    剑指offer三四题

---
+++ src/Main/NO3重建二叉树/Solution.java
@@ -0,0 +1,27 @@
+package Main.NO3重建二叉树;
+
+
+public class Solution {
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if(pre==null||in==null){
+            return null;
+        }
+        return constructCore(pre,0,pre.length-1,in,0,in.length-1);
+    }
+    private TreeNode constructCore(int[] pre, int startpre,int endpre, int[] in,int startin,int endin){
+        if(startpre>endpre||startin>endin){
+            return null;
+        }
+        TreeNode root=new TreeNode(pre[startpre]);
+        root.left=null;
+        root.right=null;
+        for(int i=startin;i<=endin;i++){
+            if(pre[startpre]==in[i]){
+                root.left=constructCore(pre,startpre+1,startpre+(i-startin),in,startin,i-1);
+                root.right=constructCore(pre,startpre+1+(i-startin),endpre,in,i+1,endin);
+                break;
+            }
+        }
+        return root;
+    }
+}
---
+++ src/Main/NO4用两个栈实现队列/Solution.java
@@ -0,0 +1,27 @@
+package Main.NO4用两个栈实现队列;
+
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        if(stack1.empty()){
+            stack1.push(node);
+        }else{
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+            stack1.push(node);
+        }
+        while(!stack2.empty()){
+            stack1.push(stack2.pop());
+        }
+
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+}
