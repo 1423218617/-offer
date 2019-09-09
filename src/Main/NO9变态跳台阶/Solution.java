373baa4126ed80c76d098668b87e0a677d6b4811
Administrator <hexi>
2019-09-09 17:13:31 +0800

    第九题

---
+++ src/Main/NO9变态跳台阶/Solution.java
@@ -0,0 +1,31 @@
+package Main.NO9变态跳台阶;
+
+/**
+ *
+ *
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+public class Solution {
+
+
+    public static void main(String[] args){
+        System.out.print(JumpFloorII(4));
+    }
+    public  static int JumpFloorII(int target) {
+        if(target<=0){
+            return 0;
+        }
+        if(target==1){
+            return 1;
+        }
+        if(target==2){
+            return 2;
+        }
+        int count=1;
+        for(int i=1;i<=target;i++){
+            count+=JumpFloorII(target-i);
+        }
+
+        return count;
+    }
+}
